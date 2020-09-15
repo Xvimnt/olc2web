358820af68a8f1e325a172d3b25c7fa25a979062
xvimnt <jvmonteros98@gmail.com>
2020-09-15 16:48:36 -0600

    arreglado arbol para el if

--- src/app/Components/editor/parser/Instruction/If.ts
+++ src/app/Components/editor/parser/Instruction/If.ts
@@ -3,41 +3,43 @@ import { Expression } from "../Abstract/Expression";
 import { Environment } from "../Symbol/Environment";
 import { Type } from "../Abstract/Retorno";
 
-export class If extends Instruction{
+export class If extends Instruction {
 
     public plot(count: number): string {
         let result = "node" + count + "[label=\"(" + this.line + "," + this.column + ") If\"];";
         // Hijo 1
         result += "node" + count + "1[label=\"(" + this.condition.line + "," + this.condition.column + ") Condicion\"];";
         result += this.condition.plot(Number(count + "1"));
-         // Hijo 2
-         result += "node" + count + "2[label=\"(" + this.code.line + "," + this.code.column + ") Codigo\"];";
-         result += this.code.plot(Number(count + "2"));
-          // Hijo 3
-        result += "node" + count + "3[label=\"(" + this.elsSt.line + "," + this.elsSt.column + ") ElseStatement\"];";
-        result += this.elsSt.plot(Number(count + "3"));
+        // Hijo 2
+        result += "node" + count + "2[label=\"(" + this.code.line + "," + this.code.column + ") Codigo\"];";
+        result += this.code.plot(Number(count + "2"));
+        // Hijo 3
+        if (this.elsSt != null) {
+            result += "node" + count + "3[label=\"(" + this.elsSt.line + "," + this.elsSt.column + ") ElseStatement\"];";
+            result += this.elsSt.plot(Number(count + "3"));
+            result += "node" + count + " -> " + "node" + count + "3;";
+        }
         // Flechas
         result += "node" + count + " -> " + "node" + count + "1;";
         result += "node" + count + " -> " + "node" + count + "2;";
-        result += "node" + count + " -> " + "node" + count + "3;";
         return result;
     }
 
-    constructor(private condition : Expression, private code : Instruction, private elsSt : Instruction | null,
-        line : number, column : number){
+    constructor(private condition: Expression, private code: Instruction, private elsSt: Instruction | null,
+        line: number, column: number) {
         super(line, column);
     }
 
-    public execute(env : Environment) {
+    public execute(env: Environment) {
         const condition = this.condition.execute(env);
-        if(condition.type != Type.BOOLEAN){
-            throw {error: "La condicion no es booleana", linea: this.line, columna : this.column};
+        if (condition.type != Type.BOOLEAN) {
+            throw { error: "La condicion no es booleana", linea: this.line, columna: this.column };
         }
 
-        if(condition.value == true){
+        if (condition.value == true) {
             return this.code.execute(env);
         }
-        else{
+        else {
             return this.elsSt?.execute(env);
         }
     }
--- src/app/Components/editor/parser/Instruction/Statement.ts
+++ src/app/Components/editor/parser/Instruction/Statement.ts
@@ -2,23 +2,30 @@ import { Instruction } from "../Abstract/Instruction";
 import { Environment } from "../Symbol/Environment";
 import { errores } from "../Errores";
 
-export class Statement extends Instruction{
+export class Statement extends Instruction {
 
     public plot(count: number): string {
-        return "node" + count + "[label=\"(" + this.line + "," + this.column + ") Statement\"];";;
+        let result = "node" + count + "[label=\"(" + this.line + "," + this.column + ") Statement\"];";;
+        let temp = 1;
+        this.code.forEach(element => {
+            result += element.plot(Number(count + "" + temp));
+            result += "node" + count + " -> " + "node" + count + "" + temp + ";";
+            temp++;
+        });
+        return result;
     }
-    
-    constructor(private code : Array<Instruction>, line : number, column : number){
+
+    constructor(private code: Array<Instruction>, line: number, column: number) {
         super(line, column);
     }
 
-    public execute(env : Environment) {
+    public execute(env: Environment) {
         const newEnv = new Environment(env);
-        for(const instr of this.code){
+        for (const instr of this.code) {
             try {
                 const element = instr.execute(newEnv);
-                if(element != undefined || element != null)
-                    return element;                
+                if (element != undefined || element != null)
+                    return element;
             } catch (error) {
                 errores.push(error);
             }
